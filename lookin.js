167fb288d2035e096acca69fcac7a9576a9c53db
Uwerajojo <josianeuwera23@gmail.com>
2018-12-14 16:15:37 +0200

    add if function equilateral

--- lookin.js
+++ lookin.js
@@ -1,15 +1,18 @@
 function myTriangle() {
-    var sides1=document.getElementById("number").value;
+    var side1=document.getElementById("number").value;
     alert(side1);
     var side2=document.getElementById("decimal").value;
     alert(side2);
     var side3=document.getElementById("integer").value;
     alert(side3);
-}
-var is =
- if(a+b>c)) {
-     (a+c>b))
-     (b+c>a))
- }
+
+var is 
+ if((side1===side2)&&(side2===side3)&&(side1===side3)) 
+ {
+     alert("Equilateral")
+ }   
+    
+ 
+} 
 
 
