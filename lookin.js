223ce5b447878729b51eafe72dc9edfd3ef9ccd1
Uwerajojo <josianeuwera23@gmail.com>
2018-12-14 15:19:35 +0200

    add value side2

--- lookin.js
+++ lookin.js
@@ -1,6 +1,8 @@
 function myTriangle() {
     var sides1=document.getElementById("number").value;
     alert(side1);
+    var side2=document.getElementById("decimal").value;
+    alert(side2);
     
 }
 
