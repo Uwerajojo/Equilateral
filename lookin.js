9f135e836c58092ad7d9c8ddef8e65ba201002a1
Uwerajojo <josianeuwera23@gmail.com>
2018-12-14 13:48:32 +0200

    add second string

--- lookin.js
+++ lookin.js
@@ -2,14 +2,10 @@ function myFunction() {
     var firstString =
     document.getElementById("myNumber").value;
     alert(firstString);
-}
-function myFunction() {
-    var firstString =
+
+    var secondString =
     document.getElementById("decimal").value;
     alert(secondString);
-}
-function myFunction() {
-    var firstString =
-    document.getElementById("integer").value;
-    alert(thirdString);
-}
+
+    
+
