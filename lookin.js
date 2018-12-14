3ddd033e3895b53476cf8cd2b9ff2012c1e1ca3f
Uwerajojo <josianeuwera23@gmail.com>
2018-12-14 13:03:18 +0200

    input 25

--- lookin.js
+++ lookin.js
@@ -3,3 +3,8 @@ function myFunction() {
     document.getElementById("myNumber").value;
     alert(firstString);
 }
+function myFunction() {
+    var firstString =
+    document.getElementById("decimal").value;
+    alert(firstString);
+}
